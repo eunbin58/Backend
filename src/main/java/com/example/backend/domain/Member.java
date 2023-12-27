9ed66656cbb496bef914fbed80bccd837b769201
eunbin <eunbin2158@gmail.com>
2023-12-27 23:11:00 +0900

    member 추가

---
+++ src/main/java/com/example/backend/domain/Member.java
@@ -0,0 +1,57 @@
+package com.example.backend.domain;
+
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    @Id
+
+    private String id;
+    private String password;
+    private String name;
+    private int birthday;
+    private boolean sex;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+}
